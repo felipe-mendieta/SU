f3febfb7ad54fc1e461b7135c4ac8e8f3e394b92
nova-21 <44357843+nova-21@users.noreply.github.com>
2020-06-06 23:09:36 -0500

    Pagina-Inicio, Articulos Destacados
    Vistas en grilla y lista de productos gratuitos, mas vendidos, mas vistos.
    Edicion de lista blanca de ruta de productos.
    Acciones javascript para cambio de vista y scrolling.

--- frontend/vistas/js/plantilla.js
+++ frontend/vistas/js/plantilla.js
@@ -2,6 +2,9 @@
 PLANTILLA
 =============================================*/
 
+// Herramienta TOOLTIP
+$('[data-toggle="tooltip"]').tooltip(); 
+
 $.ajax({
 
 	url:"ajax/plantilla.ajax.php",
@@ -20,5 +23,73 @@ $.ajax({
 
 	}
 
-
 })
+
+/*=============================================
+CUADRÍCULA O LISTA
+=============================================*/
+
+var btnList = $(".btnList");
+
+for(var i = 0; i < btnList.length; i++){
+
+	$("#btnGrid"+i).click(function(){
+
+		var numero = $(this).attr("id").substr(-1);			//toma el numero del btnGrid en el cual se hace click
+
+		$(".list"+numero).hide();							//activar vista en grilla desactivar lista
+		$(".grid"+numero).show();
+
+		$("#btnGrid"+numero).addClass("backColor");			//agregar o remover la clase backColor para que el boton 
+		$("#btnList"+numero).removeClass("backColor");		//de la vista actual contenga el color	
+
+	})
+
+	$("#btnList"+i).click(function(){
+
+		var numero = $(this).attr("id").substr(-1);				//toma el numero del btnGrid en el cual se hace click
+
+		$(".list"+numero).show();								//activar vista en lista desactivar grilla
+		$(".grid"+numero).hide();
+
+		$("#btnGrid"+numero).removeClass("backColor");			//agregar o remover la clase backColor para que el boton 
+		$("#btnList"+numero).addClass("backColor");				//de la vista actual contenga el color	
+
+	})
+
+}
+
+/*=============================================
+EFECTOS CON EL SCROLL
+=============================================*/
+
+$(window).scroll(function(){
+
+	var scrollY = window.pageYOffset; //toma posicion del scroll 
+
+	if(window.matchMedia("(min-width:768px)").matches){// en ceculares no se aplica el efecto	
+
+		if(scrollY < ($(".banner").offset().top)-150){
+
+			$(".banner img").css({"margin-top":-scrollY/3+"px"})
+
+		}else{
+
+			scrollY = 0;
+		}
+
+	}	
+	
+})
+
+$.scrollUp({
+
+	scrollText:"",
+	scrollSpeed: 2000,
+	easingType: "easeOutQuint"
+
+});
+
+
+
+
